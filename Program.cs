9444586075593f97fd1d51798f5bdf0bebe336bb
ym9079389-alt <ym9079389@gmail.com>
2026-08-30 11:05:25 +0300

    Question One

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question One
+            int pages = 464;
+            bool isAvailable = true;
+            if(pages > 300 && isAvailable)
+            {
+                Console.WriteLine("You can borrow this book");
+            }
+            #endregion
         }
     }
 }
